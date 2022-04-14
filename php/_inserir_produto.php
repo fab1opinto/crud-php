0b97731c60ae5b935d88fbd786b05980189c2e69
fab1opinto <fabiopintoapt@gmail.com>
2022-04-14 17:52:27 -0300

    adicionando o projeto ao github

---
+++ php/_inserir_produto.php
@@ -0,0 +1,9 @@
+<?php
+
+$nroproduto = $_POST['nroproduto'];
+$nomeproduto = $_POST['nomeproduto'];
+$categoria = $_POST['categoria'];
+$quantidade = $_POST['quantidade'];
+$fornecedor = $_POST['fornecedor'];
+
+?>
