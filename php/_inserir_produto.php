df48a5e857ef48e36c5b6d8045a15337c53e28ba
fab1opinto <fabiopintoapt@gmail.com>
2022-04-15 10:51:18 -0300

    fazendo alterações no código

--- php/_inserir_produto.php
+++ php/_inserir_produto.php
@@ -8,9 +8,9 @@ $categoria = $_POST['categoria'];
 $quantidade = $_POST['quantidade'];
 $fornecedor = $_POST['fornecedor'];
 
-$sql = "INSERT INTO `estoque`(`nroproduto`, `nomeproduto`, `categoria`, `quantidade`, `fornecedor`) VALUES ('[$nroproduto]','$nomeproduto','$categoria', $quantidade,'$fornecedor')";
+echo $sql = "INSERT INTO `estoque`(`nroproduto`, `nomeproduto`, `categoria`, `quantidade`, `fornecedor`) VALUES ('[$nroproduto]','$nomeproduto','$categoria', $quantidade,'$fornecedor')";
 
-echo $inserir = mysqli_query($conexao,$sql);
+$inserir = mysqli_query($conexao,$sql);
 
 ?>
 
